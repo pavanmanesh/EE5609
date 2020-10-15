0b0ba3a28ed9d0b4c7fa2de4dbc9887a3eff1e4e
pavanmanesh <70571595+pavanmanesh@users.noreply.github.com>
2020-10-15 16:11:45 +0530

    Create assign8.py

---
+++ Assignment8/codes/assign8.py
@@ -0,0 +1,17 @@
+from sympy import *
+import cmath
+
+x=0
+y=-1
+x1=0
+y1=1
+x2=1
+y2=1
+z=complex(x,y)
+z1=complex(x1,y1)
+z2=complex(x2,y2)
+
+A = Matrix([[1,z],[2,2],[z1,z2]])
+print("Matrix A is",A)
+
+print("Row-Reduced form and pivot elements are",A.rref())
